6e0605823ef89bcd0601da09cce2fe744931d9af
ashanftw <ashanchathuranga3@gmail.com>
2022-12-19 00:05:24 +0530

    bug fixes

--- test/features/authentication/presentation/cubit/authentication_cubit_test.dart
+++ test/features/authentication/presentation/cubit/authentication_cubit_test.dart
@@ -1,19 +1,32 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test_rnd/features/authentication/data/models/user.dart';
 import 'package:test_rnd/features/authentication/data_sources/authentication_remote_data_source.dart';
 import 'package:test_rnd/features/authentication/presentation/cubit/authentication_cubit.dart';
+import 'package:mockito/annotations.dart';
 
+import 'authentication_cubit_test.mocks.dart';
+
+class MockAuthenticationRemoteData extends Mock
+    implements AuthenticationRemoteData {}
+
+@GenerateMocks([MockAuthenticationRemoteData])
 void main() {
+  late MockMockAuthenticationRemoteData mockAuthenticationRemoteData;
+
   late AuthenticationCubit authenticationCubit;
   setUp(() {
+    mockAuthenticationRemoteData = MockMockAuthenticationRemoteData();
     authenticationCubit =
-        AuthenticationCubit(client: AuthenticationRemoteData());
+        AuthenticationCubit(client: mockAuthenticationRemoteData);
   });
 
   test('Should show and exception for invalid credentials', () async {
     //Arrange
     String email = '';
     String password = '';
+    when(mockAuthenticationRemoteData.login(email: email, password: password))
+        .thenThrow(Exception('invalid credentials'));
 
     //Act
     await authenticationCubit.login(email: email, password: password);
@@ -27,6 +40,8 @@ void main() {
     //Arrange
     String email = 'mark@gmail.com';
     String password = 'mark123';
+    when(mockAuthenticationRemoteData.login(email: email, password: password))
+        .thenAnswer((_) => Future.value(User(age: 12, id: '1', name: 'John')));
 
     //Act
     await authenticationCubit.login(email: email, password: password);
